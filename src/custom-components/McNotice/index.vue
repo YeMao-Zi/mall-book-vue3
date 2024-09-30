680150ce8fae0f62ca9d7dd74008319fa960edde
zsj <1794541268@qq.com>
2024-09-30 18:52:21 +0800

    feat：添加公告栏配置

---
+++ src/custom-components/McNotice/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="McNotice">
+    <noticeBar></noticeBar>
+  </div>
+</template>
+
+<script setup lang="ts">
+import noticeBar from "./noticeBar.vue";
+</script>
+
+<style lang="scss" scoped></style>
